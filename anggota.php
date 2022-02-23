a45451d9195e0f666451bd96a44f0d468e4aad35
nizaraqsa12 <nizaraqsa40@gmail.com>
2022-02-23 21:26:46 +0700

    Tugas 02 - Membuat dua class baru

--- anggota.php
+++ anggota.php
@@ -7,40 +7,36 @@ class Buku {
 
 
 
-    public function pinjam() {
-        $pinjam = date("d-m-Y");
-        $tanggal_kembali = mktime(0,0,0,date("n"),date("j")+7,date("Y"));
-        $kembali = date("d-m-Y", $tanggal_kembali);
-        return $kembali;
+    public function Bergabung(){
+        $tahun_bergabung = date('Y')
     }
 
-    public function setPenulis($penulis_buku){
-        $this->penulis = $penulis_buku;
+    public function setNo_anggota($anggota_perpus){
+        $this->no_anggota = $anggota_perpus;
     }
-    public function getPenulis(){
-        return $this->penulis;
+    public function getNo_anggota($anggota_perpus){
+        return $this->no_anggota;
     }
-    public function setIsbn($isbn_buku){
-        $this->isbn = $isbn_buku;
+    public function setNama_lengkap($nama_anggota){
+        $this->nama_lengkap = $nama_anggota;
     }
-    public function getIsbn(){
-        return $this->isbn;
+    public function getNama_lengkap($nama_anggota){
+        return $this->nama_lengkap;
     }
-    public function setJudul($judul_buku){
-        $this->judul = $judul_buku;
+    public function setNo_hp($no_hp_anggota){
+        $this->no_hp = $no_hp_anggota;
     }
-    public function getJudul(){
-        return $this->judul;
+    public function getNo_hp($no_hp_anggota){
+        return $this->no_hp;
     }
-}
+}  
 
-$buku_saya = new Buku();
-$buku_saya->setPenulis("Didik Setiawan");
-$buku_saya->setIsbn(9872317652569);
-$buku_saya->setJudul("Pemrograman Web");
-$pinjam_buku_saya = $buku_saya->pinjam();
-echo "Judul Buku = ". $buku_saya->getJudul();
-echo "Penulis Buku = ". $buku_saya->getPenulis();
-echo "ISBN = ". $buku_saya->getIsbn();
-echo "Tanggal Peminjaman = ". date('d-m-Y');
-echo "Tanggal Pengembalian = ". $pinjam_buku_saya;
+$anggota_baru = new Anggota();
+$anggota_baru->setNo_anggota(01082022);
+$anggota_baru->setNama_lengkap("Nizar Aqsa Saputra");
+$anggota_baru->setNo_hp(0895365210106);
+$Bergabung_anggota_baru = $Anggota_baru->Bergabung();
+echo "No. Anggota yang bergabung = ". $anggota_baru->getNo_anggota();
+echo "Nama Lengkap Anggota Baru = ". $anggota_baru->getNama_lengkap();
+echo "No. Hp Anggota Baru = ". $anggota_baru->getNo_hp();
+echo "Tahun Anggota Tersebut bergabung = ". $Bergabung_anggota_baru;
